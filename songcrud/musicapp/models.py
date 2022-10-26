caf1ca7db086add6c8631c864ce14b5bfde2cb29
Bamgboye Taiwo <93135338+StephainG@users.noreply.github.com>
2022-10-26 10:18:35 +0100

    'Django_CRUD'

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,29 @@
+from email.policy import default
+from pyexpat import model
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length = 100)
+    last_name = models.CharField(max_length = 100)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name
+
+class Song(models.Model):
+    title = models.CharField(max_length = 50)
+    date_released = models.DateTimeField(default=datetime.today)
+    likes = models.IntegerField()
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Lyric(models.Model):
+    content = models.CharField(max_length = 8000)
+    song_id = models.ForeignKey(Song, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.content
